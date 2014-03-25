a575ded5dcd3aab20979a7f3e2046da824efa7b8
He Liu <liulonnie@gmail.com>
2014-03-25 04:41:24 -0700

    random port

--- cmd/trib-back/main.go
+++ cmd/trib-back/main.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"math/rand"
+	"strings"
+	"time"
 
 	"trib/store"
 	"triblab"
 )
 
 var (
-	addr = flag.String("addr", "localhost:9000", "backend serve address")
+	addr = flag.String("addr", "localhost:rand", "backend serve address")
 )
 
 func main() {
 	flag.Parse()
 
+	if strings.HasSuffix(*addr, ":rand") {
+		*addr = strings.TrimSuffix(*addr, ":rand")
+		rand.Seed(time.Now().UnixNano())
+		port := 10000 + int(rand.Uint32()%20000)
+		*addr = fmt.Sprintf("%s:%d", *addr, port)
+	}
+
 	s := store.NewStorage()
+
+	log.Printf("tribble backend serve on %s", *addr)
 
 	e := triblab.ServeBack(*addr, s, nil)
 	if e != nil {
