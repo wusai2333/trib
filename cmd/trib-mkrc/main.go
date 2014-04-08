aa17b97d04429630674c285708e91fd9ecd6e3cd
He Liu <liulonnie@gmail.com>
2014-04-07 17:04:16 -0700

    trib-mkrc

--- cmd/trib-mkrc/main.go
+++ cmd/trib-mkrc/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
 
+	"trib"
+	"trib/randaddr"
+)
+
+var (
+	local = flag.Bool("local", false, "always use local ports")
+	n     = flag.Int("n", 1, "number of servers")
+)
+
+func main() {
+	if *n > 100 {
+		log.Fatal(fmt.Errorf("too many servers"))
+	}
+
+	p := randaddr.RandPort()
+
+	rc := new(trib.RC)
+	rc.Backs = make([]*trib.BackAddr, *n)
+
+	if !*local {
+		const ipOffset = 211
+		for i := 0; i < *n; i++ {
+			host := fmt.Sprintf("172.22.14.%d", ipOffset+i)
+			saddr := fmt.Sprintf("%s:%d", host, p)
+			paddr := fmt.Sprintf("%s:%d", host, p+1)
+			rc.Backs[i] = &trib.BackAddr{saddr, paddr}
+
+			p += 2
+		}
+	} else {
+		// TODO:
+	}
 }
