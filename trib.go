3f25fc8c01871db97a8df36efc4debc6052fd086
He Liu <liulonnie@gmail.com>
2014-04-19 23:47:54 -0700

    logging and more specific comments on the interface

--- trib.go
+++ trib.go
@@ -22,7 +22,10 @@ type Trib struct {
 }
 
 type Server interface {
-	// Creates a user
+	// Creates a user.
+	// Returns error when the username is invalid;
+	// returns error when the user already exists.
+	// Concurrent sign ups on the same user might both succeed with no error.
 	SignUp(user string) error
 
 	// List 20 registered users.  When there are less than 20 users that
@@ -34,24 +37,39 @@ type Server interface {
 
 	// Post a tribble.  The clock is the maximum clock value this user has
 	// seen so far by reading tribbles or clock sync.
+	// Returns error when who does not exist;
+	// returns error when post is too long.
 	Post(who, post string, clock uint64) error
 
 	// List the tribs that a particular user posted.
+	// Returns error when user has not signed up.
 	Tribs(user string) ([]*Trib, error)
 
-	// Follow someone's timeline. Returns error when who == whom.
+	// Follow someone's timeline.
+	// Returns error when who == whom;
+	// returns error when who is already following whom;
+	// returns error when who is tryting to following
+	// more than trib.MaxFollowing users.
+	// returns error when who or whom has not signed up.
 	Follow(who, whom string) error
 
-	// Unfollow someone's timeline. Returns error when who == whom.
+	// Unfollow someone's timeline.
+	// Returns error when who == whom.
+	// returns error when who is not following whom;
+	// returns error when who or whom has not signed up.
 	Unfollow(who, whom string) error
 
-	// Returns true when who following whom. Returns error when who == whom.
+	// Returns true when who following whom.
+	// Returns error when who == whom.
+	// Returns error when who or whom has not signed up.
 	IsFollowing(who, whom string) (bool, error)
 
 	// Returns the list of following users.
+	// Returns error when who has not signed up.
 	Following(who string) ([]string, error)
 
 	// List the tribs of someone's following users (including himself).
+	// Returns error when user has not signed up.
 	Home(user string) ([]*Trib, error)
 }
 
